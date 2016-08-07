ca9f36c96d7edb0df4f383d343a1f79d8710f337
lyninx <dennis.pacewicz@gmail.com>
2016-08-06 21:40:52 -0400

    added public folder

---
+++ public/app.js
@@ -0,0 +1,25 @@
+(function(){
+	var app = angular.module('firefly',['ui.router', 'ngRoute']);
+
+	app.config(function ($routeProvider, $locationProvider, $stateProvider, $urlRouterProvider) {
+	  $routeProvider
+      .when("/", {
+        templateUrl: "landing.html",
+        controller: "IndexCtrl"
+      })
+      .when("/app", {
+        templateUrl: "main.html",
+        controller: "MainCtrl"
+      });
+
+	  $locationProvider.html5Mode(true);
+	});
+
+//////////////////////////////////////////////////////////////////////////////////////
+  app.controller('IndexCtrl', function($rootScope, $scope, $routeParams, $http){  
+    console.log("index controller loaded");
+  });
+	app.controller('MainCtrl', function($rootScope, $scope, $routeParams, $http){  
+    console.log("main controller loaded");
+  });
+})()
