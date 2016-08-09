a2422fa4234abf67fffa55a8f6a684d541fe2ccf
tylerzhangtextnow <tyler.zhang@enflick.com>
2016-08-09 08:50:00 -0400

    cleaned up code base

--- api.js
+++ api.js
@@ -14,51 +14,53 @@ var t = new Twitter({
 });
 
 indico.apiKey =  'c7c2cda9dc5b823f03b3a86da7dc0e18';
+var googleMap = {
+	'apiKey' : 'AIzaSyAD--KR8TukHG0Ux-o94MCFZHmOTXQhzj4'
+};
 
 router.get('/', function(req, res) {
+	// Get params
 	var location = {
 		'lat':req.query['lat'],
 		'lon':req.query['lon']
 	};
 	var radius = req.query['radius'] * 1000;
 	var keyword = req.query['keyword'];
-	var apiKey = 'AIzaSyBPfNkKjw3kTzhMP7A2GSgDHc1vqpV_Bmw';
-	request('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + location.lat + ',' + location.lon + '\&radius=' + radius + '\&keyword=' + keyword + '\&key=' + apiKey, function (error, response, body) {
+
+	var url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + location.lat + ',' + location.lon + '\&radius=' + radius + '\&keyword=' + keyword + '\&key=' + googleMap.apiKey;
+	console.log(url);
+
+	request(url , function (error, response, body) {
 		if (!error && response.statusCode == 200) {
 			var list = JSON.parse(body).results;
-			// console.log(list);
+			console.log(list);
 			var deferreds = [];
 			for (var j = 0; j < list.length; j++) {
 				(function(j2) {
 					if (list[j2] !== undefined) {
 						var deferred = jQuery.Deferred();
-						t.get('search/tweets', {q: "\""+list[j2].name+"\"", count: 5, location: location.lat+","+location.lon+","+radius+"km", result_type: "recent"}, function(err, tweets, response) {
+						t.get('search/tweets',{q: "\""+list[j2].name+"\"", count: 5, location: location.lat+","+location.lon+","+radius+"km", result_type: "recent"},function(err, tweets, response) {
 							if (!err && tweets.statuses.length !== 0) {
-								var batchInput = [];
-								for (var i = 0; i < tweets.statuses.length; i++) {
-									batchInput.push(tweets.statuses[i].text);
-								}
-								// console.log(list[j2].name);
-								// console.log(batchInput);
-								indico.sentimentHQ(batchInput)
-								.then(function(response) {
-									// console.log(response);
+								var batchInput = tweets.statuses.map(function(tweet) {
+									return tweet.text;
+								});
+								console.log('tweets', batchInput);
+								indico.sentimentHQ(batchInput).then(function(response){
+									console.log(response);
 									var sum = response.reduce(function(a, b) {
 										return a + b;
 									}, 0);
 									var average = sum / response.length;
-									// console.log(average);
 									list[j2]['tweets'] = batchInput;
 									list[j2]['score'] = average;
-								}).then(function(){
-									//console.log("before" + list[j2]['score']);
+								}).then(function() {
 									deferred.resolve();
-								}).error(function(err){
-									// console.log(err);
-									deferred.resolve();
+								}).error(function(err) {
+									console.log(err);
+									deferred.reject();
 								});
 							} else {
-								// console.log(err);
+								console.log(err);
 								deferred.resolve();
 							}
 						});
@@ -66,13 +68,14 @@ router.get('/', function(req, res) {
 					}
 				})(j);
 			}
-			//console.log('PROMISES: ', deferreds.length);
 			jQuery.when.apply(jQuery, deferreds).done(function() {
 				newlist = list.map(function(el) {
 					return (!el.score) ? Math.random() : el.score;
 				});
 				res.json(list);
 			});
+		} else {
+			console.log(error);
 		}
 	});
 });
